a48785bb7f97d55fd4ad6292992d127276d0fa1d
dmitry-ios <60504537+dmitry-ios@users.noreply.github.com>
2020-12-29 11:12:02 +0300

    Фильтрует карточки списка и показывает ссылки

--- js/script.js
+++ js/script.js
@@ -4,10 +4,33 @@ new Vue({
   el: '#app',
   data: {
     title: 'Примеры работ',
-    projects: [
+    selected: 'any',
+    options: [
+      { text: 'Все', value: 'any'},
+      { text: 'HTML', value: 'html'},
+      { text: 'CSS', value: 'css'},
+      { text: 'SCSS', value: 'scss'},
+      { text: 'Less', value: 'less'},
+      { text: 'Bootstrap', value: 'bootstrap'},
+      { text: 'JavaScript', value: 'javascript'},
+      { text: 'jQuery', value: 'jquery'},
+      { text: 'Vue.js', value: 'vuejs'},
+    ],
+    projects: [],
+    all: [
       {
         name: 'Сайт Uber Еда',
         skills: 'HTML, SCSS, JavaScript'.split(', '),
+        links: [
+          {
+            category: 'code',
+            url: 'https://github.com/dmitry-ios/uber-eats'
+          },
+          {
+            category: 'home',
+            url: 'https://naughty-minsky-37325f.netlify.app/'
+          },
+        ],
         screenshots: [
           {
             url: 'img/thumbnails/uber-eats-desktop.png',
@@ -32,6 +55,16 @@ new Vue({
       {
         name: 'Путешествие по Европе',
         skills: 'HTML, SCSS, JavaScript'.split(', '),
+        links: [
+          {
+            category: 'code',
+            url: 'https://github.com/dmitry-ios/euro-trip'
+          },
+          {
+            category: 'home',
+            url: 'https://focused-mcclintock-729c5f.netlify.app/'
+          },
+        ],
         screenshots: [
           {
             url: 'img/thumbnails/euro-trip-desktop.png',
@@ -56,6 +89,16 @@ new Vue({
       {
         name: 'IP Address Tracker',
         skills: 'HTML, SCSS, JavaScript'.split(', '),
+        links: [
+          {
+            category: 'code',
+            url: 'https://github.com/dmitry-ios/IP-Address-Tracker'
+          },
+          {
+            category: 'home',
+            url: 'https://musing-newton-2daf03.netlify.app/'
+          },
+        ],
         screenshots: [
           {
             url: 'img/thumbnails/ip-address-tracker-desktop.png',
@@ -80,6 +123,16 @@ new Vue({
       {
         name: 'Кексобукинг',
         skills: 'Преимущественно JavaScript, готовая разметка и стили'.split(', '),
+        links: [
+          {
+            category: 'code',
+            url: 'https://github.com/dmitry-ios/1483875-keksobooking-21'
+          },
+          {
+            category: 'home',
+            url: 'https://compassionate-austin-7a0844.netlify.app/'
+          },
+        ],
         screenshots: [
           {
             url: 'img/thumbnails/keksobooking-desktop.png',
@@ -92,6 +145,24 @@ new Vue({
       {
         name: 'Кэт энерджи',
         skills: 'HTML, Less, JavaScript'.split(', '),
+        links: [
+          {
+            category: 'code',
+            url: 'https://github.com/dmitry-ios/1483875-cat-energy-20'
+          },
+          {
+            category: 'home',
+            url: 'https://nifty-agnesi-d1bbc6.netlify.app/'
+          },
+          {
+            category: 'catalog',
+            url: 'https://nifty-agnesi-d1bbc6.netlify.app/catalog.html'
+          },
+          {
+            category: 'form',
+            url: 'https://nifty-agnesi-d1bbc6.netlify.app/form.html'
+          },
+        ],
         screenshots: [
           {
             url: 'img/thumbnails/cat-energy-desktop.png',
@@ -116,6 +187,20 @@ new Vue({
       {
         name: 'Нёрдс',
         skills: 'HTML, CSS, JavaScript'.split(', '),
+        links: [
+          {
+            category: 'code',
+            url: 'https://github.com/dmitry-ios/1483875-nerds-28'
+          },
+          {
+            category: 'home',
+            url: 'https://boring-jennings-6c9769.netlify.app/'
+          },
+          {
+            category: 'catalog',
+            url: 'https://boring-jennings-6c9769.netlify.app/catalog.html'
+          },
+        ],
         screenshots: [
           {
             url: 'img/thumbnails/nerds-desktop.png',
@@ -128,6 +213,16 @@ new Vue({
       {
         name: 'Transhub 24',
         skills: 'HTML, Less, JavaScript, немного jQuery'.split(', '),
+        links: [
+          {
+            category: 'code',
+            url: 'https://github.com/dmitry-ios/Transhub24'
+          },
+          {
+            category: 'home',
+            url: 'https://compassionate-lumiere-28ffc2.netlify.app/'
+          },
+        ],
         screenshots: [
           {
             url: 'img/thumbnails/transhub-24-desktop.png',
@@ -152,6 +247,20 @@ new Vue({
       {
         name: 'Седона',
         skills: 'HTML, CSS'.split(', '),
+        links: [
+          {
+            category: 'code',
+            url: 'https://github.com/dmitry-ios/sedona'
+          },
+          {
+            category: 'home',
+            url: 'https://inspiring-mahavira-ed0a4e.netlify.app/'
+          },
+          {
+            category: 'catalog',
+            url: 'https://inspiring-mahavira-ed0a4e.netlify.app/catalog.html'
+          },
+        ],
         screenshots: [
           {
             url: 'img/thumbnails/sedona-desktop.png',
@@ -164,6 +273,16 @@ new Vue({
       {
         name: 'Код и Магия',
         skills: 'Преимущественно JavaScript, готовая верстка'.split(', '),
+        links: [
+          {
+            category: 'code',
+            url: 'https://github.com/dmitry-ios/1483875-code-and-magick-21'
+          },
+          {
+            category: 'home',
+            url: 'https://clever-wozniak-bbc709.netlify.app/'
+          },
+        ],
         screenshots: [
           {
             url: 'img/thumbnails/code-and-magick-desktop.png',
@@ -176,6 +295,16 @@ new Vue({
       {
         name: 'Meeting Ended Modal',
         skills: 'HTML, CSS, JavaScript'.split(', '),
+        links: [
+          {
+            category: 'code',
+            url: 'https://github.com/dmitry-ios/template-example/tree/master/MeetingEndedModal'
+          },
+          {
+            category: 'home',
+            url: 'https://jovial-curran-1fdece.netlify.app/'
+          },
+        ],
         screenshots: [
           {
             url: 'img/thumbnails/meeting-ended-modal-desktop.png',
@@ -188,6 +317,16 @@ new Vue({
       {
         name: 'Sign In Modal',
         skills: 'HTML, CSS'.split(', '),
+        links: [
+          {
+            category: 'code',
+            url: 'https://github.com/dmitry-ios/template-example/tree/master/SignIn'
+          },
+          {
+            category: 'home',
+            url: 'https://focused-khorana-6c2795.netlify.app/'
+          },
+        ],
         screenshots: [
           {
             url: 'img/thumbnails/sign-in-modal-desktop.png',
@@ -200,6 +339,16 @@ new Vue({
       {
         name: 'The Great Keksby',
         skills: 'HTML, CSS'.split(', '),
+        links: [
+          {
+            category: 'code',
+            url: 'https://github.com/dmitry-ios/the-great-keksby'
+          },
+          {
+            category: 'home',
+            url: 'https://sleepy-ramanujan-ebf175.netlify.app/'
+          },
+        ],
         screenshots: [
           {
             url: 'img/thumbnails/great-keksby-desktop.png',
@@ -210,5 +359,43 @@ new Vue({
         ]
       }
     ]
+  },
+  created: function () {
+    this.projects = this.all;
+  },
+  watch: {
+    selected: function (newSelected, oldSelected) {
+      this.selectNewProjects();
+    }
+  },
+  methods: {
+    titleByType: function (type) {
+      switch (type) {
+        case 'code':
+          return 'Код';
+        case 'home':
+          return 'Главная';
+        case 'catalog':
+          return 'Каталог';
+        case 'form':
+          return 'Форма';
+        default:
+          return 'Ссылка';
+      }
+    },
+    selectNewProjects: function () {
+      if (this.selected === 'any') {
+        this.projects = this.all;
+        return;
+      }
+
+      const currentSelected = this.options.find((item) => {
+        return item.value === this.selected;
+      });
+
+      this.projects = this.all.filter(function (item, index, array) {
+        return item.skills.join(',').indexOf(currentSelected.text) !== -1;
+      });
+    }
   }
 });
